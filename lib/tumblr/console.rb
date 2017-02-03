99938ca09096c38937c001a0496e0b322b7aa17b
HSU Lynch <Gernischt@gmail.com>
2017-02-03 14:58:20 +0800

    Update console.rb

--- lib/tumblr/console.rb
+++ lib/tumblr/console.rb
@@ -2,17 +2,17 @@
 require 'io/console'
 
 module Console
-  
+
   module_function
-  
+
   def draw_process_bar(now = 0, total = 1)
     rows, cols = IO.console.winsize
     percent = (now * 100.0 / total).round
     header = "#{"% 4d" % percent}% ["
     cols = cols - header.length - 2
     len = (cols / 100.0 * percent).round
-    body = if now.zero?
-      "#{" " * (cols - len)}] "
+    body = if len.zero?
+       "#{" " * (cols - len)}] "
     else
       "#{"=" * (len - 1)}#{percent < 100 ? '>' : '='}#{" " * (cols - len)}] "
     end
@@ -20,5 +20,5 @@ module Console
     print header + body
     print "\n" if now >= total
   end
-  
+
 end
