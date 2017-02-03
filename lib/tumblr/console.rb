4bbbe69b8050f3721a75b61539566ef55293b2de
HSU Lynch <Gernischt@gmail.com>
2017-02-03 13:58:46 +0800

    Create console.rb

---
+++ lib/tumblr/console.rb
@@ -0,0 +1,16 @@
+
+require 'io/console'
+
+module Console
+  def draw_process_bar(now, total)
+    rows, cols = IO.console.winsize
+    percent = (now * 100.0 / total).round
+    header = "#{"% 4d" % percent}% ["
+    cols = cols - header.length - 2
+    len = (cols / 100.0 * percent).round
+    body = "#{"=" * (len - 1)}#{percent < 100 ? '>' : '='}#{" " * (cols - len)}] "
+    print "\r"
+    print header + body
+    print "\n" if now >= total
+  end
+end
