bbefe2d7717a1a1abcf58063e24d07fb5eb3cf4a
HSU Lynch <Gernischt@gmail.com>
2017-02-03 15:06:50 +0800

    Update console.rb

--- lib/tumblr/console.rb
+++ lib/tumblr/console.rb
@@ -6,6 +6,9 @@ module Console
   module_function
 
   def draw_process_bar(now = 0, total = 1)
+    if now < 0 || total <= 0 || (now > total)
+      raise "wrong arguments: now => #{now}, total => #{total}"
+    end
     rows, cols = IO.console.winsize
     percent = (now * 100.0 / total).round
     header = "#{"% 4d" % percent}% ["
